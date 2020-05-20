4582a29cbbdafb7516643cf804d9a49b0204fd85
Felix0830 <ddjunshuhong@126.com>
2020-05-20 23:08:30 +0800

    init

---
+++ CSharpConsoleDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpConsoleDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
