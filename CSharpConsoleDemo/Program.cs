3569060741374b8c7f078162f5e9cf2cd625fd5f
shi.zhigui <shi.zhigui@synyi.com>
2021-04-13 16:32:43 +0800

    update

--- CSharpConsoleDemo/Program.cs
+++ CSharpConsoleDemo/Program.cs
@@ -6,7 +6,7 @@ namespace CSharpConsoleDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");    
         }
     }
 }
