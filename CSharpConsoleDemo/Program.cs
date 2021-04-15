8216b6232bf33e025f81251ace054fa6e7b296e2
shi.zhigui <shi.zhigui@synyi.com>
2021-04-15 17:05:31 +0800

    update

--- CSharpConsoleDemo/Program.cs
+++ CSharpConsoleDemo/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using Newtonsoft.Json;
 
 namespace CSharpConsoleDemo
 {
     class Program
     {
         static void Main(string[] args)
-        {
+        {            
             var test = new StudentTest();
 
             test.SetName("felix");
---
+++ WebApiDemo/LogHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+namespace CSharpConsoleDemo
+{
+    public class LogHelper
+    {
+        //private readonly ILogger _logger;
+        private readonly ILogger<LogHelper> _logger;
+
+        public LogHelper(ILogger<LogHelper> logger)
+        {
+            // _logger = LoggerFactory.Create(builder => { builder.AddConsole().SetMinimumLevel(LogLevel.Debug); }).CreateLogger("MyLogHelper");
+            _logger = logger; 
+
+        }
+
+        public void LogInfo(string message)
+        {
+            _logger.LogInformation($"loginformation -> {message} {DateTime.Now}");
+        }
+
+        public void LogDebug(string message)
+        {
+            _logger.LogDebug($"logdebug -> {message} {DateTime.Now}");
+        }
+    }
+}
